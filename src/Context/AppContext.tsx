7b933c48343d85cd310a90b60eae823eaf274cd6
pentekmate <90307397+pentekmate@users.noreply.github.com>
2024-10-25 14:05:43 +0200

    types in appcontext

--- src/Context/AppContext.tsx
+++ src/Context/AppContext.tsx
@@ -1,11 +1,31 @@
 import { createContext, useContext, useState } from "react";
-import { AppContextProviderProps, AppContextType } from "./AppContextTypes";
+import { AppContextProviderProps, AppContextType, Language, PreviousJob, School, Skill, Social } from "./AppContextTypes";
 
 
 const AppContext = createContext<AppContextType | null>(null);
 
 function AppProvider({children}:AppContextProviderProps){
-    const [name,setName]=useState<string | null>("null")
+    const [name,setName]=useState<string | null>(null)
+    const [email,setEmail]=useState<string | null>(null)
+    const [phoneNumber,setPhoneNumber]=useState<number | null>(null)
+    const [country,setCountry]=useState<string | null>(null)
+    const [city,setCity]=useState<string | null>(null)
+    const [jobTitle,setJobTitle]=useState<string | null>(null)
+    const [profPicture,setProfPicture]=useState<string | null>(null)
+    const [introduce,setIntroduce]=useState<string | null>(null)
+
+    const [previousJobs,setPreviousJobs]=useState<PreviousJob[] | null>(null)
+
+    const[skills,setSkills]=useState<Skill[] | null>(null)
+
+    const [schools,setSchools]=useState<School[] | null>(null)
+    
+    const[socials,setSocials]=useState<Social[] | null>(null)
+
+    const[languages,setLanguages]=useState<Language[] | null>(null)
+
+
+
     return(
         <AppContext.Provider value={{name,setName}}>
             {children}
--- src/Context/AppContextTypes.tsx
+++ src/Context/AppContextTypes.tsx
@@ -1,8 +1,70 @@
 export type AppContextType ={
     name:null | string,
-    setName:(name : string)=>void;
+    email?:string | null,
+    phoneNumber?:number| null,
+    country?:string | null,
+    city?:string | null,
+    jobTitle?:string | null,
+    profPicture?:string | null,
+    introduce?:string | null,
+    previousJobs?:PreviousJob[] | null,
+    skills?:Skill[] | null,
+    schools?:School[] | null,
+    socials?:Social[] | null,
+    languages?:Language[] | null,
+
+    setName?:(name : string)=>void;
+    setEmail?:(email : string)=>void;
+    setPhoneNumber?:(phoneNumber:number)=>void;
+    setCountry?:(country:string)=>void;
+    setCity?:(city:string)=>void;
+    setJobTitle?:(jobTitle:string)=>void;
+    setProfPicture?:(profPicture:string)=>void;
+    setIntroduce?:(introduce:string)=>void;
+
+    setPreviousJobs?:(previousJob:PreviousJob)=>void;
+
+    setSkills?:(skill:Skill)=>void;
+
+    setSchools?:(school:School)=>void;
+
+    setSocials?:(social:Social)=>void;
+
+    setLanguages?:(language:Language)=>void;
+
 }
 
 export type AppContextProviderProps = {
     children: React.ReactNode;
+}
+
+export type PreviousJob={
+    jobTitle:string,
+    employer:string,
+    beginDate:Date,
+    endDate:Date,
+    city:string,
+    description:string
+}
+
+export type Skill = {
+    skillName:string,
+    skillLevel:number
+}
+export type School={
+    schoolName:string,
+    degree:string,
+    beginDate: Date,
+    endDate:Date,
+    city:string
+}
+
+export type Social={
+    name:string,
+    link:string
+}
+
+export type Language={
+    languageName:string,
+    languageLevel:number
 }
